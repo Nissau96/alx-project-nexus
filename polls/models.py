b56065286cdf27486891a9c0705ed827fd3aeb1e
Nissua96 <ihalidu996@gmail.com>
2025-09-23 21:29:06 +0000

    feat: define database models for Poll, Choice, and Vote

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Poll Model
+class Poll(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    questions = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    expiry_date = models.DateTimeField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.questions
+
+# Choice Model.
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.choice_text
+
+# Vote Model
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+
+    class Meta:
+        unique_together = ('user', 'poll')
