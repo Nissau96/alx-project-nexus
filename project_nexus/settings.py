28824e3fd6163ce9213f5519d06ed2ae69b2750a
Nissua96 <ihalidu996@gmail.com>
2025-09-24 20:26:22 +0000

    build: add deployment build script
    This script automates the build process on Render. It ensures that all dependencies are installed and database migrations are applied before the application starts.

--- project_nexus/settings.py
+++ project_nexus/settings.py
@@ -23,6 +23,9 @@ DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
